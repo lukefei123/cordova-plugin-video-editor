56436b7cc2599d8e4ec519e7700f92d64b1bf4e4
lukefei123 <linkaidi@china-goldcard.com>
2022-07-28 17:14:58 +0800

    fix: 解决android-trancoder更新导致的问题

--- src/android/VideoEditor.java
+++ src/android/VideoEditor.java
@@ -240,7 +240,7 @@ public class VideoEditor extends CordovaPlugin {
                     float videoHeight = Float.parseFloat(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
 
                     MediaTranscoder.getInstance().transcodeVideo(fin.getFD(), outputFilePath,
-                            new CustomAndroidFormatStrategy(videoBitrate, fps, width, height), listener, videoDuration);
+                            new CustomAndroidFormatStrategy(videoBitrate, fps, width, height), listener);
 
                 } catch (Throwable e) {
                     Log.d(TAG, "transcode exception ", e);
